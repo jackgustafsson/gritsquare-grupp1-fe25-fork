4bcf527d145949eaac24dc59852320571184b0fb
Charlie <challe-1992@hotmail.com>
2026-03-18 11:16:17 +0100

    update

--- js/firebase.js
+++ js/firebase.js
@@ -1,9 +1,10 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js'
-// TODO: Add SDKs for Firebase products that you want to use
+import {
+  initializeApp,
+  getDatabase
+} from 'https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js'
+
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: 'AIzaSyBYrqmRZmIQjtWQZMSynD0F2wZPWA462tc',
   authDomain: 'messageboard-77286.firebaseapp.com',
@@ -14,6 +15,6 @@ const firebaseConfig = {
   messagingSenderId: '262402990271',
   appId: '1:262402990271:web:724586d400e56438b0a60a'
 }
-
-// Initialize Firebase
 const app = initializeApp(firebaseConfig)
+const database = getDatabase(app)
+console.log(database)
